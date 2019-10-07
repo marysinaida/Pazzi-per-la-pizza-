fa167c3ca6ba7ed59b3175885dfdc2a12f729f77
marysinaida <marydorcassinaida54@gmail.com>
2019-10-07 10:41:34 +0300

    third commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -59,7 +59,17 @@ $(document).ready(function () {
             }
         }
 
-    }
-    )}
-)
+    });
+
+
+
+$('form#contactform').submit(function(event){
+    event.preventDefault();
+    var name = $('#name').val();
+    var pass = $('#email').val();
+    var mess = $('#message').val();
+    alert("Hello " + name + " ,Thank You for Contacting Us.");
+});
+});
+
 
